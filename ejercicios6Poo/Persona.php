a511ae1203197e92829e02805f9338ca3a39f32b
Leo <joseleonelesp@gmail.com>
2024-10-21 21:33:55 +0200

    2110

---
+++ ejercicios6Poo/Persona.php
@@ -0,0 +1,16 @@
+<?php
+class Persona {
+    private $dni;
+    private $nombre ="";
+    private $email="";
+    public function __construct($dni,$nombre,$email){
+        setDni($dni);
+        setNombre($nombre);
+        setEmail($email);
+    }
+    function setDni()
+
+}
+
+
+?>
